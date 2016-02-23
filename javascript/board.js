aa61fecf52a96db1b27f17b0fd9a1176a58a328e
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-02-23 14:45:40 -0800

    completed everything except playAgain

--- javascript/board.js
+++ javascript/board.js
@@ -1,14 +1,10 @@
 (function(){
 	window.boardLogic = {};
-
 	/* this function creates each piece on the board. Each piece is an object with coordinates and a state.
 	
 	STATE: can either be null, 'X' or 'O'. When the first player clicks on an empty square, the squares state should be changed to 'X', when the second player clicks on an empty square, the state should be changed to 'O'.
 
-	COORDINATES: The coordintes are the string value of the column and row.
-
-	Here's an example:
-
+	COORDINATES: The coordinates are the string value of the column and row.
 	[[{}, {}, {name: 'piece', coordinates: '02', state: null}] 
 	 [{}, {}, {}] 
 	 [{}, {}, {}]]
@@ -19,22 +15,32 @@
 			coordinates: coordinates,
 			state: null,
 		};
-
 		return piece;
 	};
 	/* 
+	Our Board is an object that does a couple of things:
 
-	Here we create a board constructor which has all the logic we'll need to make this thing work.
+	-Holds the turnCount (Board.turnCount). The turnCount tells the program whether its player 1 or player 2's turn.
 
-	Our Board is an object that does a couple of things: 
-
-	1. Holds the turnCount (Board.turnCount). The turnCount tells the program whether its player 1 or player 2's turn.
-
-	2. Board.board is the matrix itself. calling Board.resetBoard() creates a new 3 by 3 matrix where the states of each object are set to null.
-
+	-Board.board is the matrix itself. calling Board.resetBoard() creates a new 3 by 3 matrix where the states of each object are set to null.
 	*/ 
+
+	boardLogic.create = function(num){
+		var board = _.range(num);
+		// var board = [0, 1, 2]
+		_.each(board, function(space, index, board){
+			var row = _.map(_.range(num), function(val){
+			// creates row of 3 new pieces with coordinates
+			return boardLogic.createPiece(index.toString() + val.toString());
+			});
+			board[index] = row;
+		});
+		return board;
+	};
+
 	boardLogic.createBoard = function(){	
 		var Board = {
+			// Holds the turnCount. The turnCount tells the program whether its player 1 or player 2's turn
 			turnCount: 2,
 			increment: function(){
 				this.turnCount++
@@ -46,32 +52,20 @@
 				this.board = boardLogic.create(3);
 			}
 		};
-
-		Board.resetBoard()
+		// calling Board.resetBoard() creates a new 3 by 3 matrix where the states of each object are set to null
+		Board.resetBoard();
 		
 		return Board;
 	};
-
-
-	boardLogic.create = function(num){
-		var board = _.range(num);
-		_.each(board, function(space, index, board){
-			row = _.map(_.range(num), function(val){
-			 return boardLogic.createPiece(index.toString() + val.toString());
-			});
-			board[index] = row;
-		});
-
-		return board;
-	};
-
+	// creates a new game with a fresh board
 	window.Board = boardLogic.createBoard();
 
+	// find the square within the board
 	boardLogic.findSquare = function(board, coordinates){
-	  var result;
-	  _.each(board, function(row){
-	  	if (result === undefined) {
-	  		result = _.find(row, function(square){
+		var result;
+	  	_.each(board, function(row){
+	  		if (result === undefined) {
+	  			result = _.find(row, function(square){
 	  			return square.coordinates === coordinates;
 	  		});
 	  	}
@@ -79,42 +73,176 @@
 	  if (result === undefined) {
 	  	result = "square not found";
 	  }
-
 	  return result;
 	};
 
+// ========= Helper functions to solve =============
 
-
-// ======================== Helper functions to solve ====================================
-	
+// when a square is clicked and its state changes to X, its coordinates will be pushed into the xArray
+var xArray = [];
+// when a square is clicked and its state changes to O, its coordinates will be pushed into the oArray
+var oArray = [];
+// when any square is clicked, its coordinates will be pushed into the xoArray 
+var xoArray = [];
 
 	boardLogic.updateState = function(currentSquare, boardObject){
-		// check the squares state. If it is player 1's turn, change the current squares state to X. and return the squares state.
-		// if it is player 2's turn, change the current squares state to 'O' and return the squares state.
-		// if the square has already been used, return 'current square filled'
-
-	};
-
-	boardLogic.catsGame = function(board){
-		// look through the board return true if all of the squares have been filled but no one has won.
+		// check the squares state to make sure it  is not already filled
+		var state = currentSquare.state;
+		// if it is player 1's turn, change the current squares state to X. and return the squares state
+		if(state === null && boardObject.turnCount%2===0){
+			// increment the turnCount
+ 			boardObject.increment();
+ 			// push the coordinates of the current square into xArray
+ 			xArray.push(currentSquare.coordinates);
+ 			// push the coordinates of the current square into xoArray
+ 			xoArray.push(currentSquare.coordinates);
+ 			// change the state of the current square to X and return state
+ 			currentSquare.state = 'X';
+ 			return state = 'X';
+ 		// if it is player 2's turn, change the current squares state to 'O' and return the squares state
+ 		} else if(state === null && boardObject.turnCount%2 !== 0){
+ 			// increment the turnCount
+ 			boardObject.increment();
+ 			// push the coordinates of the current square into xArray
+ 			oArray.push(currentSquare.coordinates);
+ 			// push the coordinates of the current square into xoArray
+ 			xoArray.push(currentSquare.coordinates);
+			// change the state of the current square to O and return state
+ 			currentSquare.state = 'O';
+ 			return state = 'O';
+ 		// if the square has already been used, return 'current square filled'
+ 		} else {
+ 			return 'current square filled';
+ 		}
 	};
 
 	boardLogic.checkHorizontalWin = function(currentSquare, board){
 	// if every square in the currentSquares row has the same state, that player has won.
+	/*loop through xArray and oArray
+	if array = ["00","11",""] OR ["10","11","12"] OR ["20","21","22"]
+	return true	
+	*/
+		// loop through xArray
+		var char0 = _.filter(xArray, function(element){
+			return element.charAt(0) === '0'});
+		if(char0.length === 3){
+			return true;
+		}
+		char0 = _.filter(xArray, function(element){
+			return element.charAt(0) === '1'});
+		if(char0.length === 3){
+			return true;
+		}
+		char0 = _.filter(xArray, function(element){
+			return element.charAt(0) === '2'});
+		if(char0.length === 3){
+			return true;
+		}
+		// loop through oArray
+		char0 = _.filter(oArray, function(element){
+			return element.charAt(0) === '0'});
+		if(char0.length === 3){
+			return true;
+		}
+		char0 = _.filter(oArray, function(element){
+			return element.charAt(0) === '1'});
+		if(char0.length === 3){
+			return true;
+		}
+		char0 = _.filter(oArray, function(element){
+			return element.charAt(0) === '2'});
+		if(char0.length === 3){
+			return true;
+		}
 	};
 
 	boardLogic.checkVerticalWin = function(currentSquare, board){
-	// if every square in the currentSquares column has the same state, that player has won.
+		// if every square in the currentSquares column has the same state, that player has won.
+		/*loop through xArray and oArray
+		if array = ["00","10","20"] OR ["01","11","21"] OR ["02","12","22"]
+		return true	
+		*/
+		// loop through xArray
+		var char1 = _.filter(xArray, function(element){
+			return element.charAt(1) === '0'});
+		if(char1.length === 3){
+			return true;
+		}
+		char1 = _.filter(xArray, function(element){
+			return element.charAt(1) === '1'});
+		if(char1.length === 3){
+			return true;
+		}
+		char1 = _.filter(xArray, function(element){
+			return element.charAt(1) === '2'});
+		if(char1.length === 3){
+			return true;
+		}
+		// loop through oArray
+		char1 = _.filter(oArray, function(element){
+			return element.charAt(1) === '0'});
+		if(char1.length === 3){
+			return true;
+		}
+		char1 = _.filter(oArray, function(element){
+			return element.charAt(1) === '1'});
+		if(char1.length === 3){
+			return true;
+		}
+		char1 = _.filter(oArray, function(element){
+			return element.charAt(1) === '2'});
+		if(char1.length === 3){
+			return true;
+		}
+	};
+
+	boardLogic.checkDiagonalWin = function(currentSquare, board){
+		// if every square diagonal to the currentSquare is has the same state, that player has won.
+		/*loop through xArray and oArray
+		if array = ["00,"11","22"]
+		return true	
+		*/
+		// if the two integer digits in the coordinate are the same, it is a square in the top left to bottom right diagonal. check xArray
+		var charMatch = _.filter(xArray, function(element){
+			return element.charAt(0) === element.charAt(1)});
+		if(charMatch.length === 3){
+			return true;
+		}
+		// check oArray
+		charMatch = _.filter(oArray, function(element){
+			return element.charAt(0) === element.charAt(1)});
+		if(charMatch.length === 3){
+			return true;
+		}
+		/*loop through xArray and oArray
+		if array =["20","11","02"]
+		return true	
+		*/
+		// if the two integer digits in the coordinate add up to 2, it is a square in the top right to bottom left diagonal. check xArray.
+		var charSum = _.filter(xArray, function(element){
+			return parseInt(element.charAt(0)) + parseInt(element.charAt(1)) === 2});
+		if(charSum.length === 3){
+			return true;
+		}
+		// check oArray
+		var charSum = _.filter(oArray, function(element){
+			return parseInt(element.charAt(0)) + parseInt(element.charAt(1)) === 2});
+		if(charSum.length === 3){
+			return true;
+		}
+	};
+
+	boardLogic.catsGame = function(currentSquare, board){
+		// look through the board return true if all of the squares have been filled but no one has won.
+		if(xoArray.length === 9){
+			return true;
+		}
 	};
 
 	boardLogic.playAgain = function(prompt, board){
-		// if the players prompt is true, clear the state from each square in the board.
+		// if the players prompt is true, clear the state from each square in the board back to null
+		if(prompt){
+			return Board.resetBoard();
+		}
 	};
-	
-	
-	boardLogic.checkDiagonalWin = function(currentSquare, board){
-		// if every square diagonal to the currentSquare is has the same state, that player has won.
-	};
-
-
 })();
--- javascript/gameLogic.js
+++ javascript/gameLogic.js
@@ -1,35 +1,72 @@
+// do not need to modify
 $(document).ready(function(){
 	var boardObject = window.Board;
+	// holds the contents of the entire game and its history
+	/* var boardObject = {
+		turnCount: 2,
+		increment: function(){
+			this.turnCount++
+		},
+		resetTurnCount: function(){
+			this.turnCount = 2;
+		},
+		resetBoard: function(){
+			this.board = boardLogic.create(3);
+		}
+		board: [[Object, Object, Object,],
+			   [Object, Object, Object],
+			   [Object, Object, Object]],
+	}
+	*/
+	// access just the matrix of the window.Board object
 	var Board = window.Board.board;
-	var TurnCount = window.Board.turn;
+	/*
+	var Board: [[Object, Object, Object,],
+			   [Object, Object, Object],
+			   [Object, Object, Object]],
+	*/
+	// access just the turnCount function in window.Board object
+	var TurnCount = window.Board.turnCount;
+	// was var TurnCount = window.Board.turn;
+	// think that was an error
 
 	$('.square').on('click', function(e){
-
 		var coordinate = this.attributes.id.nodeValue;
+		/* number printed when click on square
+			00 01 02
+			10 11 12
+			20 21 22
+		*/
 		var squareObject = boardLogic.findSquare(Board, coordinate);
+		/* squareObject = {
+			name: "piece", 
+			coordinates: "12", 
+			state: null}
+		*/
 		var currentState = squareObject.state;
+
 		if (squareObject){
 			currentState = boardLogic.updateState(squareObject, boardObject);
 			if (currentState === 'X') {
 				this.classList.add('X');
 			} else if (currentState === 'O'){
 				this.classList.add('O');
-			} else if (currentState === "square filled"){
-				alert('square already has been used');
+			} else if (currentState === "current square filled"){
+				alert('Square already has been used');
 			}
 		}
 
 		if (boardLogic.checkHorizontalWin(squareObject, Board)){
-			var userPrompt = confirm('you win! would you like to play again?');
+			var userPrompt = confirm('You win! Would you like to play again?');
 		    boardLogic.playAgain(userPrompt, boardObject)
 		} else if (boardLogic.checkVerticalWin(squareObject, Board)){
-			var userPrompt = confirm('you win! would you like to play again?');
+			var userPrompt = confirm('You win! Would you like to play again?');
 		    boardLogic.playAgain(userPrompt, boardObject);
 		} else if (boardLogic.checkDiagonalWin(squareObject, Board)){
-			var userPrompt = confirm('you win! would you like to play again?');
+			var userPrompt = confirm('You win! Would you like to play again?');
 		    boardLogic.playAgain(userPrompt, boardObject)
-		} else if (boardLogic.catsGame(Board)){
-			var userPrompt = confirm('you win! would you like to play again?');
+		} else if (boardLogic.catsGame(squareObject, Board)){
+			var userPrompt = confirm('Cats game! No one won. Would you like to play again?');
 		    boardLogic.playAgain(userPrompt, boardObject)
 		}
 	});
